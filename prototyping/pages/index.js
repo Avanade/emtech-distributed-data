3d5f319262f47d74e982875113496e123a7a43e1
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-26 17:16:05 +0100

    feat(prototyping): added next.js repo

---
+++ prototyping/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Next.js empty repo</h1>
+
+        <p>
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <footer>Footer goes here.</footer>
+    </div>
+  );
+}
