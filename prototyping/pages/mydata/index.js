45ccda2b75a18dd6f398a54d726cd4401de069a6
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-27 11:52:24 +0100

    feat(sql-ledger): mock-up of data feed and timeline

--- prototyping/pages/mydata/index.js
+++ prototyping/pages/mydata/index.js
@@ -8,12 +8,21 @@ export default function MyData() {
           <TableIcon className="-ml-1 mr-1 h-5 w-5" aria-hidden="true" />
           <h1 className="text-3xl font-bold leading-tight text-gray-900">
             My Data
-          </h1>
+          </h1>{" "}
+          <a
+            href="/mydata/feed"
+            type="button"
+            className="inline-flex ml-8 items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            View Sharing Feed
+          </a>
         </div>
       </header>
       <main>
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="px-4 py-8 sm:px-0">Add content here.</div>
+          <div className="px-4 py-8 sm:px-0">
+            You are sharing the following data:
+          </div>
         </div>
       </main>
     </div>
