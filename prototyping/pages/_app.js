09f9a6b85ceb769e8e15aeb0d2b454507180f744
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-27 10:07:42 +0100

    feat(sql-ledger): updated Nav bar to display current path name

--- prototyping/pages/_app.js
+++ prototyping/pages/_app.js
@@ -1,6 +1,10 @@
 import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 
+// Support SSR for webcam component in scan.js
+if (typeof window === "undefined") global.window = {};
+if (typeof global.navigator === "undefined") global.navigator = {};
+
 function LedgerApp({ Component, pageProps }) {
   return (
     <Layout>
