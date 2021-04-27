d1cc593b2da3e52fc7a711cb5a7f41f37c254fae
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-27 10:50:15 +0100

    WIP: added routing URLs for mydata and share

--- prototyping/components/layout.tsx
+++ prototyping/components/layout.tsx
@@ -3,6 +3,7 @@ import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { QrcodeIcon } from "@heroicons/react/solid";
 import { useRouter } from 'next/router';
+import Image from 'next/image'
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -13,6 +14,17 @@ const navigation = [
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+function isCurrentPage(href, pathname) {
+  if  (href==="/") {
+    if (pathname === "/") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return (pathname.search(href) === 0);
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -39,12 +51,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto"
-                    src="oltiva-logo.png"
+                    src="/oltiva-logo.png"
                     alt="Oltiva"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="oltiva-logo.png"
+                    src="/oltiva-logo.png"
                     alt="Oltiva"
                   />
                 </div>
@@ -54,10 +66,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                       key={item.name}
                       href={item.href}
                       className={classNames(
-
-                        (router.pathname == item.href) ? 'border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                        isCurrentPage(item.href,router.pathname) ? 'border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
                       )}
-                      aria-current={(router.pathname == item.href) ? 'page' : undefined}
+                      aria-current={isCurrentPage(item.href,router.pathname) ? 'page' : undefined}
                     >
                       {item.name}
                     </a>
@@ -87,9 +98,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                       key={item.name}
                       href={item.href}
                       className={classNames(
-                        (router.pathname == item.href) ? 'bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6'
+                        isCurrentPage(item.href,router.pathname) ? 'bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6'
                       )}
-                      aria-current={(router.pathname == item.href) ? 'page' : undefined}
+                      aria-current={isCurrentPage(item.href,router.pathname) ? 'page' : undefined}
                     >
                       {item.name}
                     </a>
