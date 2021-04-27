e367a2a40fe7a346030fb67171bea134dcea4831
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-27 11:35:04 +0100

    feat(sql-ledger): qR code scanner redirects to sharing page for valid scans

--- prototyping/pages/scan.js
+++ prototyping/pages/scan.js
@@ -1,9 +1,39 @@
 import { QrcodeIcon } from "@heroicons/react/solid";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import { useRouter } from "next/router";
 
 export default function Scan() {
-  const [data, setData] = useState("No QR code found");
+  const router = useRouter();
+  const [displayData, setDisplayData] = useState("No QR code found.");
+  const [instruction, setInstruction] = useState(
+    "Point your device's camera at a QR code to detect it."
+  );
+  const [qrCode, setQrCode] = useState("");
+  const [validQrCode, setQrCodeValidity] = useState(false);
+
+  useEffect(() => {
+    if (qrCode.search("actif") === 0) {
+      setQrCodeValidity(true);
+      setInstruction(
+        "You have scanned a valid data sharing code. If you would like to share your data, open the data sharer."
+      );
+      router.push("/share/" + qrCode);
+      setDisplayData(
+        <>
+          <a
+            href={"/share/" + qrCode}
+            type="button"
+            className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span>Open Data Sharer</span>
+          </a>
+        </>
+      );
+    } else if (qrCode !== "") {
+      setDisplayData(<>Not a valid Oltiva Actif QR code.</>);
+    }
+  });
 
   return (
     <div className="py-10">
@@ -18,17 +48,19 @@ export default function Scan() {
       <main>
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="px-4 py-8 sm:px-0">
-            Point your device's camera at a QR code to detect it.
-            <div className="place-content-center">
+            {instruction}
+            <div className={validQrCode ? "hidden" : "place-content-center"}>
               <BarcodeScannerComponent
                 className="w-1/3"
                 onUpdate={(err, result) => {
-                  if (result) setData(result.text);
-                  else setData("No QR code found");
+                  if (result) {
+                    setQrCode(result.text);
+                  }
                 }}
               />
             </div>
-            <p>{data}</p>
+            <p className={validQrCode ? "hidden" : "italic"}>{qrCode}</p>
+            <p>{displayData}</p>
           </div>
         </div>
       </main>
