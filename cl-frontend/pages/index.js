0d58336afaf2d1de8f16e22ae8fb41a152631356
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-28 16:12:27 +0100

    feat(confidential-ledger): added Oltiva Safedrive layout

--- cl-frontend/pages/index.js
+++ cl-frontend/pages/index.js
@@ -1,32 +1,33 @@
 import {
-  ClockIcon,
   EyeIcon,
-  LibraryIcon,
-  ShieldExclamationIcon,
+  RssIcon,
+  TruckIcon,
+  LockClosedIcon,
+  BeakerIcon,
 } from "@heroicons/react/outline";
 
 const features = [
   {
-    name: "Control",
-    description: "You choose what data to share, and how it should be used.",
-    icon: LibraryIcon,
+    name: "Security",
+    description:
+      "Ensure your vehicle is securely monitored, without tampering.",
+    icon: LockClosedIcon,
   },
   {
-    name: "Timeliness",
+    name: "Rules of the Road",
     description:
-      "No indefinite sharing. Choose how long to share your data for.",
-    icon: ClockIcon,
+      "Know your safety - track your distance from vehicles around you.",
+    icon: TruckIcon,
   },
   {
     name: "Visibility",
-    description: "See how, where, and why your data was used.",
+    description: "Share your data with insurers on a need to know basis.",
     icon: EyeIcon,
   },
   {
     name: "The Hot Seat",
-    description:
-      "Control your data at any time. Anonymity, blanking, or wiping... it's yours after all.",
-    icon: ShieldExclamationIcon,
+    description: "See your driving history and telemetry in a real-time feed.",
+    icon: RssIcon,
   },
 ];
 
@@ -35,22 +36,32 @@ export default function Home() {
     <>
       <div className="py-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center">
-          <h1 className="text-3xl font-bold leading-tight text-gray-900">
-            Oltiva Actif
+          <h1 className="text-3xl font-bold leading-tight text-purple-900">
+            Oltiva SafeDrive
           </h1>
         </div>
       </div>
-      <div className="bg-indigo-700">
+      <div className="bg-purple-700">
         <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
           <h2 className="text-3xl font-extrabold text-white tracking-tight">
-            Privacy is Healthy
+            Connected Vehicles are en-route
           </h2>
-          <p className="mt-4 max-w-3xl text-lg text-indigo-200">
-            We're generating more data every day, in the journey to the
-            quantified self. But we don't know how our data is used, or even if
-            we'll end up in an ad. Use Oltiva Actif to control your data, and
-            know how it's used - privacy is healthy.
+          <p className="mt-4 max-w-3xl text-lg text-purple-200">
+            The rules of the road are changing. Drivers can be human, or they
+            can be robotic. How do we ensure safety? How do we attribute
+            responsibility? Oltiva SafeDrive uses state of the art Confidential
+            Ledger technology to securely store the state of the car and the
+            surrounding environment, to assign responsibility, and preserve
+            privacy.
           </p>
+          <a
+            type="button"
+            href="/simulate"
+            className="mt-2 inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          >
+            <BeakerIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+            Watch the simulation
+          </a>
           <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name}>
@@ -66,7 +77,7 @@ export default function Home() {
                   <h3 className="text-lg font-medium text-white">
                     {feature.name}
                   </h3>
-                  <p className="mt-2 text-base text-indigo-200">
+                  <p className="mt-2 text-base text-purple-200">
                     {feature.description}
                   </p>
                 </div>
