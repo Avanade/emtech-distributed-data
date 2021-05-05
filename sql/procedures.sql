ac099e19d73670aaf073031034c05ecf9f01594f
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-05-05 17:33:29 +0100

    style(sql-ledger): added new procedures to example SQl

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -5,16 +5,20 @@ from
   [dbo].[Oltiva_Partners] A
   inner join [dbo].[Oltiva_QR] B on A.PartnerId = B.PartnerId
 where
-  B.QRlocId = * Search ID * -- insert Heart Rate
+  B.QRlocId = * Search
+ID *
+-- insert Heart Rate
 INSERT INTO
-  [dbo].[Oltiva_DataPoints] (DataPointId, DataSetId, DataTimestamp, DataValue)
+  [dbo].[Oltiva_DataPoints]
+  (DataPointId, DataSetId, DataTimestamp, DataValue)
 VALUES
   (
-    * NewUniwuieId *,
-    * DatasetId *,
-    * Timenow *,
+    * NewUniwuieId *, 
+    * DatasetId *, 
+    * Timenow *, 
     * BPMmeasure *
-  ) -- Revoke Access
+  )
+-- Revoke Access
 DELETE FROM
   [dbo].[Oltiva_QR]
 WHERE
@@ -26,3 +30,17 @@ FROM
   [dbo].[Oltiva_DataPoints]
 WHERE
   DataSetId = * INPUTId *
+-- Get user DataSets
+select
+  A.*,
+  B.[Unit], B.[DataType], B.[SharedUntilDateTime], B.[DataStatus]
+from
+  [dbo].[Oltiva_Partners] A
+  inner join [dbo].[Oltiva_DataSet] B on A.PartnerId = B.PartnerId
+WHERE * user
+id*
+-- Read user ledger
+select A.*, B.[Unit], B.[DataType], B.[LastUpdateDateTime], B.[SharedUntilDateTime], B.[ledger_sequence_number], B.[DataStatus], B.[ledger_operation_type_desc]
+from [dbo].[Oltiva_Partners] A inner join [dbo].[Oltiva_DataSet_Ledger] B on A.PartnerId = B.PartnerId
+WHERE B.UserId = @param_user_id
+ORDER BY B.[ledger_sequence_number];
