c7c10ed0447f64c132c1b2084e57a38e90aee7b2
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-05-05 12:26:49 +0100

    feat(sql-ledger): added ShareTable component, closes #46

--- sql-ledger-demo/components/BasicPage.tsx
+++ sql-ledger-demo/components/BasicPage.tsx
@@ -3,7 +3,7 @@ import HeaderTitle from "./HeaderTitle";
 export default function BasicPage(props) {
     return (
         <div className="py-10">
-            <HeaderTitle title={props.title} icon={props.icon} />
+            <HeaderTitle title={props.title} icon={props.icon}>{props.headerChildren}</HeaderTitle>
             <main>
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="px-4 py-8 sm:px-0">
---
+++ sql-ledger-demo/components/ShareTable.tsx
@@ -0,0 +1,34 @@
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function ShareTable(props) {
+    const shared = Array.from(props.data);
+    if (shared.length === 0) return <b>Error: No data returned from server</b>;
+    return (
+        <div>
+            <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                {shared.map((dataElement) => (
+                    <li key={dataElement.id} className="col-span-1 flex shadow-sm rounded-md">
+                        <div
+                            className={classNames(
+                                dataElement.bgColor,
+                                'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
+                            )}
+                        >
+                            {dataElement.initials}
+                        </div>
+                        <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                            <div className="flex-1 px-4 py-2 text-sm truncate">
+                                <span className="text-gray-900 font-medium hover:text-gray-600">
+                                    {dataElement.type}
+                                </span>
+                                <p className="text-gray-500">{dataElement.target}</p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
