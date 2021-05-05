a707eb4b102c9a3fe819f03f1c98c09f7ce46c55
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-05-05 16:26:13 +0100

    feat(sql-ledger): added QR code API

---
+++ sql-ledger-demo/pages/api/qr/[qid].ts
@@ -0,0 +1,16 @@
+import { readQrCode, deleteQrCode } from '@/lib/sqlledger'
+
+export default function qrHandler({ query, method }, res) {
+  let qid = query.qid;
+  switch (method) {
+    case 'GET':
+      readQrCode(qid).then(result => res.status(200).json(result.recordsets));
+      break
+    case 'DELETE':
+      deleteQrCode(qid).then(result => res.status(200).json(result.recordsets));
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
