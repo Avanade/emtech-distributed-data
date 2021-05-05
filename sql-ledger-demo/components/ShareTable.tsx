b22b0e87e792f4cd70646263ce53f3020a36f497
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-05-05 12:34:18 +0100

    fix(sql-ledger): change components to use array rather than dot notation

--- sql-ledger-demo/components/ShareTable.tsx
+++ sql-ledger-demo/components/ShareTable.tsx
@@ -9,21 +9,21 @@ export default function ShareTable(props) {
         <div>
             <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 {shared.map((dataElement) => (
-                    <li key={dataElement.id} className="col-span-1 flex shadow-sm rounded-md">
+                    <li key={dataElement['id']} className="col-span-1 flex shadow-sm rounded-md">
                         <div
                             className={classNames(
-                                dataElement.bgColor,
+                                dataElement['bgColor'],
                                 'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
                             )}
                         >
-                            {dataElement.initials}
+                            {dataElement['initials']}
                         </div>
                         <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                             <div className="flex-1 px-4 py-2 text-sm truncate">
                                 <span className="text-gray-900 font-medium hover:text-gray-600">
-                                    {dataElement.type}
+                                    {dataElement['type']}
                                 </span>
-                                <p className="text-gray-500">{dataElement.target}</p>
+                                <p className="text-gray-500">{dataElement['target']}</p>
                             </div>
                         </div>
                     </li>
--- sql-ledger-demo/components/Timeline.tsx
+++ sql-ledger-demo/components/Timeline.tsx
@@ -9,7 +9,7 @@ export default function Timeline(props) {
     if (timeline.length === 0) return <b>Error: No data returned from server</b>;
     return (
         timeline.map((event, eventIdx) => (
-            <li key={event.id}>
+            <li key={event['id']}>
                 <div className="relative pb-8">
                     {eventIdx !== timeline.length - 1 ? (
                         <span
@@ -34,18 +34,18 @@ export default function Timeline(props) {
                         <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
                             <div>
                                 <p className="text-sm text-gray-500">
-                                    {event.content}{" "}
+                                    {event['content']}{" "}
                                     <a
-                                        href={event.href}
+                                        href={event['href']}
                                         className="font-medium text-gray-900"
                                     >
-                                        {event.target}
+                                        {event['target']}
                                     </a>
                       .
                     </p>
                             </div>
                             <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                                <time dateTime={event.datetime}>{event.date}</time>
+                                <time dateTime={event['datetime']}>{event['date']}</time>
                             </div>
                         </div>
                     </div>
