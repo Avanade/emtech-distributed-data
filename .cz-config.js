23e7e38ab4ddb92cf1a2345aeb4c38e659ac2010
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-06-07 12:11:07 +0100

    ci(commitizen): updated cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -5,13 +5,11 @@ module.exports = {
     { value: "docs", name: "docs:     Documentation only changes" },
     {
       value: "style",
-      name:
-        "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
+      name: "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
     },
     {
       value: "refactor",
-      name:
-        "refactor: A code change that neither fixes a bug nor adds a feature",
+      name: "refactor: A code change that neither fixes a bug nor adds a feature",
     },
     {
       value: "perf",
@@ -20,14 +18,13 @@ module.exports = {
     { value: "test", name: "test:     Adding missing tests" },
     {
       value: "ci",
-      name:
-        "ci:       Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
+      name: "ci:       Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
     },
     { value: "revert", name: "revert:   Revert to a commit" },
     { value: "WIP", name: "WIP:      Work in progress" },
   ],
 
-  scopes: [ { name: "sql-ledger" },{ name: "confidential-ledger" }],
+  scopes: [{ name: "confidential-ledger" }, { name: "sql-ledger" }],
 
   allowTicketNumber: false,
   isTicketNumberRequired: false,
@@ -47,8 +44,7 @@ module.exports = {
     // used if allowCustomScopes is true
     customScope: "Denote the SCOPE of this change:",
     subject: "Write a short description of the change:\n",
-    body:
-      'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: "List any BREAKING CHANGES (optional):\n",
     footer:
       "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
