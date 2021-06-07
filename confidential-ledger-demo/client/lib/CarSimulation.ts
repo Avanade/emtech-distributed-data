171bf0cdab908c68874d662c77c143bcd1fc9dcf
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-06-07 16:11:17 +0100

    feat(confidential-ledger): car simulation to move cars over a grid

--- confidential-ledger-demo/client/lib/CarSimulation.ts
+++ confidential-ledger-demo/client/lib/CarSimulation.ts
@@ -1,4 +1,5 @@
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuid4} from 'uuid';
+import { DateTime } from 'luxon';
 
 type canContain = Car | null;
 
@@ -6,7 +7,7 @@ interface Car {
     licencePlate: string;
     sprite: string;
     id: string;
-    location: { x: number, y: number};
+    location: { x: number, y: number };
 }
 
 interface GridSquare {
@@ -14,9 +15,9 @@ interface GridSquare {
 }
 
 interface Grid {
-    x_dimension: number;
-    y_dimension: number;
-    grid_squares: GridSquare[][];
+    xDimension: number;
+    yDimension: number;
+    gridSquares: GridSquare[][];
 }
 
 class Car implements Car {
@@ -27,28 +28,46 @@ class Car implements Car {
     constructor(licencePlate: string, sprite: string) {
         this.licencePlate = licencePlate;
         this.sprite = sprite;
-        this.id = uuidv4();
+        this.id = uuid4();
     }
 }
 
 class GridSquare implements GridSquare {
     squareContains: canContain;
-    constructor() {
+    xCoordinate: number;
+    yCoordinate: number;
+
+    constructor(xCoordinate: number, yCoordinate: number) {
         this.squareContains = null;
+        this.xCoordinate=xCoordinate;
+        this.yCoordinate=yCoordinate;
     }
 
-    setField(newField: Car | null):void {
+    setField(newField: Car | null): void {
         if (this.squareContains instanceof Car) {
             throw new Error("Tried to put a car where a car already exists.");
         }
         this.squareContains = newField;
     }
 
-    getField():Car| null{
+    getField(): Car | null {
         return this.squareContains;
     }
 
-    getFieldType():string {
+    updateCoordinates(xCoordinate:number, yCoordinate: number): void {
+        this.xCoordinate=xCoordinate;
+        this.yCoordinate=yCoordinate;
+    }
+
+    hasCar(): boolean {
+        if (this.getFieldType() == "Car") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getFieldType(): string {
         if (this.squareContains instanceof Car) {
             return "Car";
         } else {
@@ -58,91 +77,129 @@ class GridSquare implements GridSquare {
 }
 
 class Grid implements Grid {
-    x_dimension: number;
-    y_dimension: number;
-    grid_squares: GridSquare[][];
-    constructor(x_dimension: number, y_dimension: number) {
-        this.x_dimension = x_dimension;
-        this.y_dimension = y_dimension;
-        this.grid_squares = this.InitGrid(x_dimension, y_dimension);
+    xDimension: number;
+    yDimension: number;
+    gridSquares: GridSquare[][];
+
+    constructor(xDimension: number, yDimension: number) {
+        this.xDimension = xDimension;
+        this.yDimension = yDimension;
+        this.gridSquares = this.initGrid(xDimension, yDimension);
     }
 
-    InitGrid(x_dimension: number, y_dimension: number): GridSquare[][] {
-        let grid_squares: GridSquare[][]=[];
-        for(var x: number = 0;x< x_dimension; x++) {
-            grid_squares[x] = [];
-            for(var y: number = 0; y< y_dimension; y++) {
-                grid_squares[x][y] = new GridSquare();
-            }
+    initGrid(xDimension: number, yDimension: number): GridSquare[][] {
+        let gridSquares: GridSquare[][] = [];
+        for (let x: number = 0; x < xDimension; x++) {
+            gridSquares[x] = this.initRow(x,yDimension);
         }
-        return grid_squares;
+        return gridSquares;
     }
 
-    CountEmpty(): number {
-        let grid: GridSquare[][] = this.grid_squares;
-        let empty:number = 0;
-        for(let x: number = 0; x< this.x_dimension; x++) {
-            for (let y: number = 0; y < this.y_dimension; y++) {
-                let grid_square: GridSquare = this.grid_squares[x][y];
-                if (grid_square.getFieldType() ===  'null') {
-                    empty++;
+    initRow(xDimension:number, yDimension: number):GridSquare[] {
+        let newRow=[];
+        for (let y: number = 0; y < yDimension; y++) {
+            newRow[y] = new GridSquare(xDimension,y);
+        }
+        return newRow;
+    }
+
+    getCarSquares():GridSquare[][] {
+        let carSquares: GridSquare[][] = [];
+        for (let x: number = 0; x < this.xDimension; x++) {
+            carSquares[x]=[];
+            this.gridSquares[x].forEach((yElement) => {
+                if (yElement.hasCar()) {
+                    let yCoOrd=yElement.yCoordinate;
+                    carSquares[x][yCoOrd]=yElement;
                 }
-            }
+            })
         }
-        return empty;
+        return carSquares;
     }
 }
 
 
 class CarSimulation {
     grid: Grid;
-    max_cars: number;
     timer: ReturnType<typeof setInterval>;
 
 
-    constructor(max_cars: number, tickLengthMilliseconds: number) {
-        this.max_cars = max_cars;
-        this.grid = new Grid(4, 4);
+    constructor(tickLengthMilliseconds: number) {
+        this.grid = new Grid(4, 10);
         this.initSimulation(tickLengthMilliseconds);
     }
 
     initSimulation(tickLengthMilliseconds: number) {
-        let startingCars = this.randomNumber(1, this.max_cars);
-        this.grid = this.addCars(startingCars, this.grid);
-        this.timer = setInterval(() => this.runTimeStep(), tickLengthMilliseconds)
+        let startingCars = this.randomNumber(this.grid.xDimension);
+        this.addCars(startingCars);
+        this.timer = setInterval(() => this.runTimeStep(), tickLengthMilliseconds);
+        console.log("Simulation started, running every %sms.",tickLengthMilliseconds);
+        this.printGrid();
     }
 
-    addCars(carAmountToAdd: number, grid: Grid): Grid {
-        let total_squares = grid.x_dimension * grid.y_dimension;
-        if (carAmountToAdd > total_squares) {
-            throw new Error("Asked for more cars than there's space for.");
+    addCars(carAmountToAdd: number): void {
+        let startingColumn=[...this.grid.gridSquares[0]];
+        startingColumn=startingColumn.sort( () => .5 - Math.random() );//not truly random, but this isn't crypto
+        let carsLeftToAdd=carAmountToAdd;
+        while (carsLeftToAdd>0) {
+            let nextSquare=startingColumn.shift();
+            let newCar:Car=new Car("LICENCE","nosprite"); // TODO: Add licence plates and sprits
+            let yCoOrd:number=nextSquare.yCoordinate;
+            let gridSquare:GridSquare=this.grid.gridSquares[0][yCoOrd];
+            gridSquare.setField(newCar);
+            carsLeftToAdd--;
         }
-        let empty_square = grid.CountEmpty();
-        if (carAmountToAdd > empty_square) {
-            throw new Error("Asked for more cars than there's empty squares.");
-        }
-        return grid;
     }
 
-    randomNumber(min: number, max:number):number {
-        return Math.random() * (max - min) + min;
+    randomNumber(max: number): number {
+        return Math.floor(Math.random() * max) + 1;
     }
 
     runTimeStep() {
-        let currentCars = this.countCars();
-        if (currentCars<this.max_cars) {
-
-        }
-
+        console.log("Time step. %s",DateTime.now().toLocaleString(DateTime.TIME_24_WITH_SECONDS));
+        this.grid=this.moveVehicles();
+        this.addCars(this.randomNumber(this.grid.xDimension));
+        this.printGrid();
     }
 
-    countCars() {
-        return 10;
+    printGrid() {
+        let gridSquares=this.grid.gridSquares;
+        gridSquares.slice().reverse().forEach((element,index) => {
+            let printString="-" + index + "-";
+            element.forEach((yElement,yIndex) => {
+                if (yElement.hasCar()) {
+                    printString+="c";
+                } else {
+                    printString+="-";
+                }
+            });
+            console.log(printString);
+        })
     }
 
-    moveVehicles() { }
+    moveVehicles():Grid {
+        let xWidth=this.grid.xDimension;
+        let yHeight=this.grid.yDimension;
+        let nextTick = new Grid(xWidth, yHeight);
+        let carSquares=this.grid.getCarSquares();
+
+        carSquares.forEach((xColumn) => {
+           xColumn.forEach((yGridSquare)=> {
+              let targetX=yGridSquare.xCoordinate+1;
+              if (targetX<xWidth) {
+                  let targetY=yGridSquare.yCoordinate;
+                  yGridSquare.updateCoordinates(targetX,targetY);
+                  nextTick.gridSquares[targetX][targetY]=yGridSquare;
+              }
+           });
+        });
+        return nextTick;
+    }
 
     endSimulation() {
         clearInterval(this.timer);
+        console.log("Simulation ended.");
     }
 }
+
+let testSimulation = new CarSimulation( 500);
