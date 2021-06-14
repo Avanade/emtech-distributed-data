3192460f0bb03d3e44e7a22ae1b15fc02b3b4d18
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-06-14 16:02:36 +0100

    WIP: resolves domain lookup bug with next.js and docker

--- confidential-ledger-demo/client/pages/api/ledger/[uuid].ts
+++ confidential-ledger-demo/client/pages/api/ledger/[uuid].ts
@@ -1,13 +1,13 @@
 import { readLedger, appendLedger } from "@/lib/cl-api";
 
-export default async function dataHandler({ query, method }, res) {
+export default async function dataHandler({ query, method, body }, res) {
     let uuid = query.uuid;
     switch (method) {
         case 'GET':
             readLedger(uuid).then(result => res.status(200).json(result));
             break;
         case 'POST':
-            appendLedger(uuid, req.body).then(result => res.status(200).json(result));
+            appendLedger(uuid, body).then(result => res.status(200).json(result));
             break
         default:
             res.setHeader('Allow', ['GET', 'POST']);
