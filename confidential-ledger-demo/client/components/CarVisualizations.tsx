40780864564ece6e355142c5cb7c8b13e2354ffd
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-06-24 15:38:22 +0100

    feat(confidential-ledger): adds random car color selection and car metadata viz

--- confidential-ledger-demo/client/components/CarVisualizations.tsx
+++ confidential-ledger-demo/client/components/CarVisualizations.tsx
@@ -1,33 +1,47 @@
 import { Grid, GridSquare } from "@/lib/CarSimulation"
-import React from "react";
+import React, {useState} from "react";
 
 
 export function MotorwayGrid(props) {
     let grid : Grid = props.grid;
     let gridSquares : GridSquare[][] =grid.gridSquares;
+    let displayModalFunction=props.displayModalFunction;
+    let [currentCar, setCurrentCar] = useState("");
     return (<>
-        <div className="max-w-md bg-gray-300 grid grid-cols-5 gap-0 divide-x divide-white divide-dashed">
-            {gridSquares.slice().reverse().map((element) => (
-                    element.map((yElement) => (
-                        <CarIcon key={yElement.getCoordId()} squareContains={yElement.hasCar() ? "car" : "nothing"} color="text-purple-600"/>
-                    ))
-            ))}
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+            <div className="bg-gray-50 px-4 py-5 sm:p-6 grid-cols-2 grid">
+                <div className="max-w-md bg-gray-300 grid grid-cols-5 gap-0 divide-x divide-white divide-dashed">
+                    {gridSquares.slice().reverse().map((element) => (
+                            element.map((yElement) => (
+                                <CarIcon focusElement={setCurrentCar} displayModalFunction={displayModalFunction} field={yElement.getField()} key={yElement.getCoordId()} squareContains={yElement.hasCar() ? "car" : "nothing"} />
+                            ))
+                    ))}
+                </div>
+                <div>
+                    <h3 className="text-xl font-medium text-gray-900">{currentCar}</h3>
+                </div>
+            </div>
         </div>
     </>);
 }
 
 export function CarIcon(props) {
-    return props.squareContains == "car" ? (
-        <div className="{props.color}">
+    if (props.squareContains === "car") {
+        let displayModalFunction=props.displayModalFunction;
+        let carClasses="cursor-pointer inline-block " + props.field.carColor;
+        let carTitle = props.field.licencePlate;
+        let carData=props.field.metadata;
+
+        return <div onMouseEnter={() => props.focusElement(carTitle)} onMouseLeave={() => props.focusElement("")} onClick={() => displayModalFunction(carTitle,carData)} className={carClasses}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                  stroke="currentColor">
                 <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z"/>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"/>
             </svg>
-        </div>
-    ) : (
-        <div>&nbsp;</div>
-    );
+        </div>;
+    } else {
+        return <div>&nbsp;</div>;
+    }
 }
 
