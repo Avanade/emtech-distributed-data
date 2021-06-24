249f354684d9fd6b7c32cd93384ff7f6892592ec
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-06-24 18:12:23 +0100

    feat(confidential-ledger): adds colours for production nextjs build

--- confidential-ledger-demo/client/components/CarVisualizations.tsx
+++ confidential-ledger-demo/client/components/CarVisualizations.tsx
@@ -60,13 +60,15 @@ function buildData(carSimData:CarSimData) {
 
 export function CarIcon(props) {
     if (props.squareContains === "car") {
-        let displayModalFunction=props.displayModalFunction;
-        let carClasses="cursor-pointer inline-block " + props.field.carColor;
+        let displayModalFunction = props.displayModalFunction;
+        let carClasses = "cursor-pointer inline-block " + props.field.carColor;
         let carTitle = props.field.licencePlate;
-        let carData=props.field.metadata;
-        let carId=props.field.id;
+        let carData = props.field.metadata;
+        let carId = props.field.id;
 
-        return <div onMouseEnter={() => props.focusElement(carTitle, buildData(carData))} onMouseLeave={() => props.focusElement("",<></>)} onClick={() => displayModalFunction(carTitle,carId)} className={carClasses}>
+        return <div onMouseEnter={() => props.focusElement(carTitle, buildData(carData))}
+                    onMouseLeave={() => props.focusElement("", <></>)}
+                    onClick={() => displayModalFunction(carTitle, carId)} className={carClasses}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                  stroke="currentColor">
                 <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z"/>
@@ -74,6 +76,8 @@ export function CarIcon(props) {
                       d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"/>
             </svg>
         </div>;
+    } else if (false) {
+        return <div className="text-purple-600 text-blue-600 text-green-600 text-red-700 text-pink-900 text-yellow-900 text-grey-400 bg-gradient-to-r from-green-400 to-blue-500"/>
     } else {
         return <div>&nbsp;</div>;
     }
